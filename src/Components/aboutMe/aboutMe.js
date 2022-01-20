9d73b560f4fde5836ac012e95ba441d52257f6c5
Travis Cook <travismcook93@gmail.com>
2022-01-20 12:35:31 -0700

    changed color pallette away from U of M

--- src/Components/aboutMe/aboutMe.js
+++ src/Components/aboutMe/aboutMe.js
@@ -3,7 +3,7 @@ import "./aboutMe.css"
 function aboutMe(){
   return (
     <div className="my-16 mx-auto ">
-      <div className="text-white text-lg shadow-lg bg-sky-900 container rounded w-11/12 infoDiv">
+      <div className="text-white text-lg shadow-lg bg-slate-700 container rounded w-11/12 infoDiv">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi enim nunc faucibus a. Tellus mauris a diam maecenas sed.
       </div>
     </div>
--- src/Components/projects/projects.js
+++ src/Components/projects/projects.js
@@ -2,7 +2,7 @@ import "./projects.css"
 
 function projects(){
   return (
-    <div className="my-16 mx-auto bg-sky-900 rounded w-11/12 drop-shadow-xl">
+    <div className="projContainer my-16 mx-auto rounded w-11/12 drop-shadow-xl ">
       <div className="flex flex-row my-4">
         <div className="text-white mx-auto bg-slate-700 rounded basis-5/12 m-2 drop-shadow-xl">this is a space for PROJECT 1</div>
         <div className="text-white mx-auto bg-slate-700 rounded basis-5/12 m-2 drop-shadow-xl">this is a space for PROJECT 2</div>
