99f00b76baf619177e59ec6ab4e99a87ca914cb9
Travis Cook <travismcook93@gmail.com>
2022-01-21 08:16:53 -0700

    more styling changes, added chevron arrows to first two pages, to make it more intuitive

--- src/Components/aboutMe/aboutMe.js
+++ src/Components/aboutMe/aboutMe.js
@@ -1,11 +1,13 @@
 import "./aboutMe.css"
+import {ChevronDoubleDownIcon} from "@heroicons/react/outline"
 
 function aboutMe(){
   return (
-    <div className=" py-48 lg:p-2 mx-auto h-full lg:h-auto ">
-      <div className="snap-center text-white text-xl shadow-lg bg-slate-700 container rounded w-11/12 m-auto lg:my-4">
-        I am a full-stack web developer who is currently an instruction specialist at the full-stack bootcamp by Trilogy/2U education through the Georgia Institute of Technology. Thanks to this experience I am well versed in many front-end and back-end languages. When I am not working with students, I am working on some of my side coding projects, and watching sports like baseball, hockey, or the premier league.
-      </div>
+    <div className="grid-cols-1 grid-rows-2 h-screen lg:h-auto py-32">
+        <div className="p-10 mx-auto w-96 row-start-1 rounded snap-center font-semibold text-lg text-black bg-gray-50">
+          I am a full-stack web developer who is currently an instruction specialist at the full-stack bootcamp by Trilogy/2U education through the Georgia Institute of Technology. Thanks to this experience I am well versed in many front-end and back-end languages. When I am not working with students, I am working on some of my side coding projects, and watching sports like baseball, hockey, or the premier league.
+        </div>
+        <ChevronDoubleDownIcon className="row-start-2 py-12 h-36 w-32 m-auto text-white animate-pulse lg:hidden" />
     </div>
   )
 }
--- src/Components/skillBreakdown/skillBreakdown.js
+++ src/Components/skillBreakdown/skillBreakdown.js
@@ -1,15 +1,17 @@
+import {ChevronDoubleDownIcon} from "@heroicons/react/outline"
+
 function skillBreakdown(){
   return(
-    <div className="h-screen snap-center my-16 lg:my-4 mx-auto flex flex-row lg:h-auto">
-      <div className="h-3/6 my-auto text-white bg-slate-700 basis-1/3 m-2 rounded shadow-lg ">
-        this is a space for front end skills
+    <div className="h-screen lg:h-auto">
+      <div className="h-96 snap-center my-16 lg:my-4 mx-auto flex flex-row lg:h-auto">
+        <div className="h-5/6 my-auto text-white bg-slate-700 basis-1/2 m-2 rounded shadow-lg lg:h-auto ">
+          this is a space for front end skills
+        </div>
+        <div className="h-5/6 my-auto text-white bg-slate-700 basis-1/2 m-2 rounded shadow-lg lg:h-auto">
+          this is a space for back end skills
+        </div>
       </div>
-      <div className="h-3/6 my-auto text-white bg-slate-700 basis-1/3 m-2 rounded shadow-lg">
-        this is a space for back end skills
-      </div>
-      <div className="h-3/6 my-auto text-white bg-slate-700 basis-1/3 m-2 rounded shadow-lg">
-        this is a space for soft skills?
-      </div>
+      <ChevronDoubleDownIcon className="py-12 h-36 w-32 m-auto text-white animate-pulse lg:hidden" />
     </div>
   )
 }
