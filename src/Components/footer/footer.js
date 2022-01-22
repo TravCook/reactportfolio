83307e963cb257583b055359f96edf21f0a5219f
Travis Cook <travismcook93@gmail.com>
2022-01-22 12:47:52 -0700

    footer styling and links to socials added

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -3,9 +3,13 @@ import "./footer.css"
 function footer(){
   return(
     <div className="sticky bottom-0 lg:bottom-0 text-white flex flex-row bg-slate-700 rounded shadow-lg footerDiv">
-      <div className="text-left basis-1/4 m-2">contact me</div>
+      <a className="text-left basis-1/4 m-2">contact me</a>
       <div className="font-bold basis-2/4 m-2 text-xs">© NoCo Web Solutions LLC.</div>
-      <div className="basis-1/4 text-right m-2">socials</div>
+      <div className="basis-1/4 text-right m-2 grid grid-cols-3">
+        <a href="https://www.github.com/TravCook" className="fa fa-github m-auto"></a>
+        <a href="https://www.linkedin.com/in/travis-cook93/" className="fa fa-linkedin m-auto"></a>
+        <a href="https://twitter.com/NoCoWebDevelop" className="fa fa-twitter m-auto"></a>
+      </div>
     </div>
   )
 }
