da68b6077e7b8afcf21956cd673a6e59179d090c
Travis Cook <travismcook93@gmail.com>
2022-03-17 09:36:41 -0600

    changed out one project for another one

--- src/Components/projects/projects.js
+++ src/Components/projects/projects.js
@@ -1,7 +1,7 @@
 import {Link} from "react-router-dom"
 import "./projects.css"
 import placeholder from "../../public/placeholder.png"
-import dndtextbased from "../../public/dndTextBased.png"
+import reactPWGen from "../../public/reactPWGen.png"
 import homeworkTracker from '../../public/homeworkTracker.png'
 
 function projects(){
@@ -11,14 +11,14 @@ function projects(){
         <div className="flex flex-row">
           <div className="text-white mx-auto bg-slate-700 rounded basis-5/12 m-2 drop-shadow-xl">
             <div className="flex-row">
-              <div className="text-xl my-3">Text Based DnD</div>
+              <div className="text-xl my-3">Random Password Generator</div>
               <div >
-                <img className=" rounded mx-auto w-12/12 lg:w-3/5" src={dndtextbased} />
+                <img className=" rounded mx-auto w-12/12 lg:w-3/5" src={reactPWGen} />
                 <div className="flex-row my-4">
-                <a href="https://github.com/TravCook/DnDTextBased" target="_blank">
+                <a href="https://github.com/TravCook/reactPWGen" target="_blank">
                 <button className="mx-4 bg-emerald-900 p-4 rounded w-4/12">REPO</button>
                 </a>
-                <a href="https://master.dh3gd75gqzoxx.amplifyapp.com/" target="_blank">
+                <a href="https://reactpasswordgenerator.herokuapp.com/" target="_blank">
                   <button className="mx-4 bg-emerald-900 p-4 rounded w-4/12">website</button>
                 </a>
               </div>
