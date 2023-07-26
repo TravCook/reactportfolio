f93337f12ff47f06cc1f6e7688a67393fd1f6e2f
Travis Cook <tcook@instructors.2u.com>
2023-07-25 18:07:27 -0600

    possible casing issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import AboutMeShort from './components/aboutMeShort/aboutMeShort';
-import Github from './components/github/github';
-import FeaturedProjects from './components/featuredProjects/featuredProjects';
-import SkillsShort from './components/skillsShort/skillsShort';
+import AboutMeShort from './Components/aboutMeShort/aboutMeShort';
+import Github from './Components/github/github';
+import FeaturedProjects from './Components/featuredProjects/featuredProjects';
+import SkillsShort from './Components/skillsShort/skillsShort';
 import {Container, Row, Col} from 'react-bootstrap'
-import Footer from "./components/footer/footer.js"
+import Footer from "./Components/footer/footer.js"
 
 function App() {
   return (
