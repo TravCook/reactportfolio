510f3c6b016083b41d1ac634f01be100a563b44e
Travis Cook <tcook@instructors.2u.com>
2023-10-31 11:27:07 -0600

    download is working locally, testing deployable solutions

--- src/Components/navbar/navbar.js
+++ src/Components/navbar/navbar.js
@@ -5,7 +5,7 @@ import resume from '../../public/Travis_Cook_Resume.docx'
 const Navbar = () => {
     return (
         <Row className='navbarStyle'>
-            <Col lg='2' style={{margin: 'auto'}}> <a style={{fontSize: 22, color: '#F5F5F5'}} download="TravisCookResume" href={resume}>My Resume</a>
+            <Col lg='2' style={{margin: 'auto'}}> <a style={{fontSize: 22, color: '#F5F5F5'}} download href={resume}>My Resume</a>
             </Col>
             <Col style={{fontSize: '42px'}}>WHO IS TRAVIS COOK</Col>
             <Col lg='2'>
