080581d124ae531a8c88661d40e0f2cfe05fddc5
Travis Cook <tcook@instructors.2u.com>
2023-10-31 10:50:59 -0600

    added resume download

--- src/Components/navbar/navbar.js
+++ src/Components/navbar/navbar.js
@@ -1,10 +1,11 @@
-import {Row, Col, DropdownButton} from 'react-bootstrap'
+import {Row, Col, Button} from 'react-bootstrap'
 import './navbar.css'
+import resume from '../../public/Travis_Cook_ResumePDF.pdf'
 
 const Navbar = () => {
     return (
         <Row className='navbarStyle'>
-            <Col lg='2' style={{margin: 'auto'}}> RESUME
+            <Col lg='2' style={{margin: 'auto'}}> <a style={{fontSize: 22, color: '#F5F5F5'}} download href={resume}>My Resume</a>
             </Col>
             <Col style={{fontSize: '42px'}}>WHO IS TRAVIS COOK</Col>
             <Col lg='2'>
